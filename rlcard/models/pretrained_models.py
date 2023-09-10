bdafd61654b09a7334a1461e0ecf54f838b4953d
gsiatras <gsiatras@isc.tuc.gr>
2023-09-10 19:20:45 +0300

    all except PER are running

--- rlcard/models/pretrained_models.py
+++ rlcard/models/pretrained_models.py
@@ -29,7 +29,7 @@ class LeducHoldemCFRModel(Model):
               functioning well.
         '''
         return [self.agent, self.agent]
-
+#
 class LimitHoldemDQNModel(Model):
     ''' A pretrained model on Leduc Holdem with CFR (chance sampling)
     '''
